923fe2637fd674f094acda16f6ba3342b8c9c851
Murtaza Narwarwala <murtazanarwar@gmail.com>
2024-11-08 09:04:45 +0530

    User Button Created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 "use client";
-import { useAuthActions } from "@convex-dev/auth/react"
 import { UserButton } from "./auth/user-button";
 
 export default function Home() {
-  const { signOut } = useAuthActions();
   return (
     <div>
       <UserButton />
