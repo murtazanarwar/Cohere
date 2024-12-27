228f15af91d8fec3f7f47835b329420234bd2418
Murtaza Narwarwala <murtazanarwar@gmail.com>
2024-12-27 12:27:45 +0530

    message api updated

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -5,10 +5,14 @@ import { useChannelId } from "@/hooks/use-channel-id";
 import { Loader, TriangleAlert } from "lucide-react";
 import { Header } from "./header";
 import { ChatInput } from "./chat-input";
+import { useGetMessages } from "@/features/messages/api/use-get-messages";
 
 
 const ChannelPage = () => {
     const channelId = useChannelId();
+    
+    
+    const { results } = useGetMessages({ channelId });
     const { data: channel , isLoading: channelLoading } = useGetChannel({ id: channelId });
 
     if(channelLoading){
@@ -31,7 +35,9 @@ const ChannelPage = () => {
     return (
         <div className="flex flex-col h-full">
             <Header title={channel.name} />
-            <div className="flex-1" />
+            <div className="flex-1" >
+                {JSON.stringify(results)}
+            </div>
             <ChatInput
                 placeholder={`Message # ${channel.name}`}
             />
